4b79b455a9a60943a8f07477efce3c435f9a39aa
urgabel <urgabel@hotmail.com>
2013-02-10 07:47:43 +0100

    Corrected properties, reworked reading from bytes

--- source/Graphics/EffectPart.cs
+++ source/Graphics/EffectPart.cs
@@ -5,8 +5,9 @@ using System.Text;
 
 namespace HoMMol_core.Graphics
 {
-    /// <summary>Handles an Effect Part data structure
-    /// <remarks>only handles a part, not a full entry in Effe files</remarks>
+    /// <summary>Handles an Effect Part data structure, as a single frame in 
+    /// <para>a 3D Effect animation.</para>
+    /// <remarks>Used by Effect class</remarks>
     /// <example>
     /// Example of an effect part in a Effe ini file:
     /// EffectId0=50974
@@ -40,27 +41,32 @@ namespace HoMMol_core.Graphics
         /// <value>Texture Id, in 3DTexture.ini</value>
         public UInt32 TextureId;
 
-        /// <summary>Optional Scale, 100 means no scaling</summary>
-        /// <remarks>If 100%, it is not shown in ini files</remarks>
-        /// <value>Optional Scale, 100 means no scaling</value>
-        public UInt32 Scale;
+        /// <summary>Optional Scale, 1 (100%) means no scaling</summary>
+        /// <remarks>If 1 (100%), it is not shown in ini files</remarks>
+        /// <value>Optional Scale, 1 (100%) means no scaling</value>
+        public float Scale;        // By default is 1 (100%)
 
         /// <summary>Asb</summary>
         /// <value>Alfa Source Blend
         /// <seealso cref="D3DBLEND">check possible options</seealso></value>
-        public Byte Asb;        // Alfa Source Blend. Usually 5.
+        public D3DBLEND Asb;        // Alfa Source Blend. Usually 5.
 
         /// <summary>Adb</summary>
         /// <value>Alfa Destination Blend
         /// <seealso cref="D3DBLEND">check possible options</seealso></value>
-        public Byte Adb;        // Alfa Destination Blend. Usually 6.
+        public D3DBLEND Adb;        // Alfa Destination Blend. Usually 6.
 
         /// <summary>Rendering occupy space, optional, default 0. 
         /// With other 3D objects produce realistic 3D crossover.</summary>
         /// <remarks>If 0, it is not shown in ini files</remarks>
         /// <value>Rendering occupy space, with other 3D objects produce 
         /// realistic 3D crossover, optional, default 0.</value>
-        public UInt16 ZBuffer;
+        public Boolean ZBuffer;    // Internally is a boolean
+
+        /// <summary>Billboard type</summary>
+        /// <remarks>If 0, it is not shown in ini files</remarks>
+        /// <value>Billboard type</value>
+        public Boolean Billboard;
         #endregion
 
         #region Constructor
@@ -69,24 +75,26 @@ namespace HoMMol_core.Graphics
         {
             EffectId = 0;
             TextureId = 0;
-            Scale = 100;
-            Asb = 5;
-            Adb = 6;
-            ZBuffer = 0;
+            Scale = 1;
+            Asb = D3DBLEND.D3DBLEND_SRCALPHA;
+            Adb = D3DBLEND.D3DBLEND_INVSRCALPHA;
+            ZBuffer = false;
+            Billboard = false;
         }
 
-        /// <summary>New Effect Part from provided fields, 
-        /// all fields, but ZBuffer is optional, default = 0</summary>
+        /// <summary>New Effect Part from provided fields, all fields 
+        /// but Billboard is optional</summary>
         /// <param name="E">Effect Id, in 3DEffectObj.ini</param>
         /// <param name="T">Texture Id, in 3DTexture.ini</param>
-        /// <param name="S">Scale, 100 means no scaling</param>
+        /// <param name="S">Scale, 1 (100%) means no scaling</param>
         /// <param name="As">Asb, Alpha Source Blending mode
         /// <seealso cref="D3DBLEND">check possible options</seealso></param>
         /// <param name="Ad">Adb, Alpha Destination Blending mode
         /// <seealso cref="D3DBLEND">check possible options</seealso></param>
-        /// <param name="Z">Optional, Rendering occupy space, default 0</param>
-        public EffectPart(UInt32 E, UInt32 T, UInt32 S,
-            Byte As, Byte Ad, UInt16 Z = 0)
+        /// <param name="Z">Rendering occupy space, default false(0)</param>
+        /// <param name="B">Optional, Billboard type, default false (0)</param>
+        public EffectPart(UInt32 E, UInt32 T, float S, D3DBLEND As, 
+            D3DBLEND Ad, Boolean Z, Boolean B = false)
         {
             EffectId = E;
             TextureId = T;
@@ -94,20 +102,57 @@ namespace HoMMol_core.Graphics
             Asb = As;
             Adb = Ad;
             ZBuffer = Z;
+            Billboard = B;
         }
 
-        /// <summary>New Effect Part from provided fields, with default 
-        /// scale = 100%, and ZBuffer optional, default = 0</summary>
+        /// <summary>New Effect Part from provided fields, all fields 
+        /// except Scale (1 -> 100% by default), and Billboard is optional 
+        /// as default = false (0)</summary>
         /// <param name="E">Effect Id, in 3DEffectObj.ini</param>
         /// <param name="T">Texture Id, in 3DTexture.ini</param>
         /// <param name="As">Asb, Alpha Source Blending mode
         /// <seealso cref="D3DBLEND">check possible options</seealso></param>
         /// <param name="Ad">Adb, Alpha Destination Blending mode
         /// <seealso cref="D3DBLEND">check possible options</seealso></param>
-        /// <param name="Z">Optional, Rendering occupy space, default 0</param>
-        public EffectPart(UInt32 E, UInt32 T, Byte As, Byte Ad, UInt16 Z = 0)
+        /// <param name="Z">Rendering occupy space, default false(0)</param>
+        /// <param name="B">Optional, Billboard type, default false (0)</param>
+        public EffectPart(UInt32 E, UInt32 T, D3DBLEND As,
+            D3DBLEND Ad, Boolean Z, Boolean B = false)
         {
-            new EffectPart(E, T, 100, As, Ad, Z);
+            new EffectPart(E, T, 1, As, Ad, Z, B);
+        }
+
+        /// <summary>New Effect Part from provided fields, all fields 
+        /// except ZBuffer as false by default (0), and Billboard
+        /// is optional, as default = false (0)</summary>
+        /// <param name="E">Effect Id, in 3DEffectObj.ini</param>
+        /// <param name="T">Texture Id, in 3DTexture.ini</param>
+        /// <param name="S">Scale, 1 (100%) means no scaling</param>
+        /// <param name="As">Asb, Alpha Source Blending mode
+        /// <seealso cref="D3DBLEND">check possible options</seealso></param>
+        /// <param name="Ad">Adb, Alpha Destination Blending mode
+        /// <seealso cref="D3DBLEND">check possible options</seealso></param>
+        /// <param name="B">Optional, Billboard type, default false (0)</param>
+        public EffectPart(UInt32 E, UInt32 T, float S, D3DBLEND As,
+            D3DBLEND Ad, Boolean B = false)
+        {
+            new EffectPart(E, T, S, As, Ad, false, B);
+        }
+
+        /// <summary>New Effect Part from provided fields, all fields 
+        /// except Scale (1 -> 100% by default) and ZBuffer as false (0), 
+        /// and Billboard is optional, as default = false (0)</summary>
+        /// <param name="E">Effect Id, in 3DEffectObj.ini</param>
+        /// <param name="T">Texture Id, in 3DTexture.ini</param>
+        /// <param name="As">Asb, Alpha Source Blending mode
+        /// <seealso cref="D3DBLEND">check possible options</seealso></param>
+        /// <param name="Ad">Adb, Alpha Destination Blending mode
+        /// <seealso cref="D3DBLEND">check possible options</seealso></param>
+        /// <param name="B">Optional, Billboard type, default false (0)</param>
+        public EffectPart(UInt32 E, UInt32 T, D3DBLEND As,
+            D3DBLEND Ad,  Boolean B = false)
+        {
+            new EffectPart(E, T, 1, As, Ad, false, B);
         }
 
         /// <summary>New Effect Part from byte array, as in dbc Effe file</summary>
@@ -123,17 +168,16 @@ namespace HoMMol_core.Graphics
             {
                 EffectId = BitConverter.ToUInt32(buffer, 0);
                 TextureId = BitConverter.ToUInt32(buffer, 4);
-                Scale = BitConverter.ToUInt32(buffer, 8);
-                Asb = buffer[12];
-                Adb = buffer[13];
-                ZBuffer = BitConverter.ToUInt16(buffer, 14);
+                Scale = BitConverter.ToSingle(buffer, 8);
+                Asb = (D3DBLEND)buffer[12];
+                Adb = (D3DBLEND)buffer[13];
+                ZBuffer = (buffer[14] != 0);
+                Billboard = (buffer[15] != 0);
             }
         }
 
         /// <summary>New Effect Part from multiline string</summary>
-        /// <param name="str">7 Lines separated by \r\n, like with this.ToString()</param>
-        /// <exception cref="ArgumentOutOfRangeException">if less than 7
-        /// strings in the array (inherited)</exception>
+        /// <param name="str">Lines separated by \r\n, like with this.ToString()</param>
         public EffectPart(String str)
         {
             String[] st = Common.SplitLines(str);
@@ -142,25 +186,16 @@ namespace HoMMol_core.Graphics
 
         /// <summary>New Effect Part from string array, as in ini files
         /// <remarks>Does not check if correct format</remarks></summary>
-        /// <param name="str">Array of 7 strings, one for each line</param>
-        /// <exception cref="ArgumentOutOfRangeException">if less than 7
-        /// strings in the array</exception>
+        /// <param name="str">Array of strings, one for each line</param>
         public EffectPart(String[] str)
         {
-            if (str.Length < 7)
-                throw new ArgumentOutOfRangeException("str", "there are too few lines; expected 7, found " + str.Length.ToString());
-            EffectId = UInt32.Parse(str[0].Split('=')[1]);
-            TextureId = UInt32.Parse(str[1].Split('=')[1]);
-            Scale = UInt32.Parse(str[2].Split('=')[1]);
-            Asb = Byte.Parse(str[3].Split('=')[1]);
-            Adb = Byte.Parse(str[4].Split('=')[1]);
-            ZBuffer = UInt16.Parse(str[5].Split('=')[1]);
+            ReadFromArray(str, 0);
         }
         #endregion
 
         #region PublicMethods
         /// <summary>Type of the class</summary>
-        /// <returns>Return "EffP"</returns>
+        /// <returns>Returns "EffP"</returns>
         public String Type()
         {
             return MAGIC_TYPE_TXT;
@@ -176,6 +211,7 @@ namespace HoMMol_core.Graphics
             if ((E.Asb != this.Asb)) return false;
             if ((E.Adb != this.Adb)) return false;
             if ((E.ZBuffer != this.ZBuffer)) return false;
+            if ((E.Billboard != this.Billboard)) return false;
             return true;
         }
 
@@ -188,19 +224,26 @@ namespace HoMMol_core.Graphics
         }
 
         /// <summary>Parse to a String with an index in each field</summary>
-        /// <remarks>Scale and ZBuffer are skipped when default values</remarks>
+        /// <remarks>Scale, ZBuffer and Billboard are skipped when 
+        /// default values</remarks>
         /// <param name="i">Optional, index, inside an Effect structure</param>
-        /// <returns>Return a string ready to save to an ini file</returns>
-        public String ToString(UInt32 i = 0)
+        /// <returns>Returns a string ready to save to an ini file</returns>
+        public String ToString(UInt32 i = 0xFFFFFFFF)
         {
             String s = "";
-            String index = i.ToString();     // By default is 0
-            s += "EffectId" + index + "=" + EffectId + "\r\n";
-            s += "TextureId" + index + "=" + TextureId + "\r\n";
-            if (Scale != 100) s += "Scale" + index + "=" + Scale + "\r\n";
-            s += "ASB" + index + "=" + Asb + "\r\n";
-            s += "ADB" + index + "=" + Adb + "\r\n";
-            if (ZBuffer != 0) s += "ZBuffer" + index + "=" + ZBuffer + "\r\n";
+            String index = "";
+            if (i != 0xFFFFFFFF) 
+                index = i.ToString();
+            s += "EffectId" + index + "=" + EffectId.ToString() + "\r\n";
+            s += "TextureId" + index + "=" + TextureId.ToString() + "\r\n";
+            if (Scale != 1) 
+                s += "Scale" + index + "=" + (100 * Scale).ToString() + "\r\n";
+            s += "ASB" + index + "=" + Asb.ToString() + "\r\n";
+            s += "ADB" + index + "=" + Adb.ToString() + "\r\n";
+            if (ZBuffer) 
+                s += "ZBuffer" + index + "=1\r\n";
+            if (Billboard) 
+                s += "Billboard" + index + "=1";
             return s;
         }
 
@@ -211,11 +254,62 @@ namespace HoMMol_core.Graphics
             Byte[] b = new Byte[16];
             Buffer.BlockCopy(BitConverter.GetBytes(EffectId), 0, b, 0, 4);
             Buffer.BlockCopy(BitConverter.GetBytes(TextureId), 0, b, 4, 4);
-            Buffer.BlockCopy(BitConverter.GetBytes(Scale), 0, b, 8, 4);
-            b[12] = Asb;
-            b[13] = Adb;
-            Buffer.BlockCopy(BitConverter.GetBytes(ZBuffer), 0, b, 14, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(Scale / 100), 0, b, 8, 4);
+            b[12] = (byte)Asb;
+            b[13] = (byte)Adb;
+            b[14] = (ZBuffer ? (byte)1 : (byte)0);
+            b[15] = (Billboard ? (byte)1: (byte)0);
             return b;
+        }
+
+        /// <summary>Read Effect Part from string array, as in ini files
+        /// <remarks>Does not check if correct format</remarks></summary>
+        /// <param name="str">Array of strings, one for each line</param>
+        /// <param name="i">Optional, Array index to start reading from
+        /// by default is zero</param>
+        /// <returns>next index to read</returns>
+        public int ReadFromArray(String[] str, int i = 0)
+        {
+            int j = str.Length;
+            Scale = 100;
+            ZBuffer = false;
+            Billboard = false;
+            if (i < j && str[i].StartsWith("EffectId"))
+            {
+                EffectId = UInt32.Parse(str[i].Split('=')[1]);
+                i++;
+            }
+            if (i < j && str[i].StartsWith("TextureId"))
+            {
+                TextureId = UInt32.Parse(str[i].Split('=')[1]);
+                i++;
+            }
+            if (i < j && str[i].StartsWith("Scale"))
+            {
+                Scale = float.Parse(str[i].Split('=')[1]) / 100;
+                i++;
+            }
+            if (i < j && str[i].StartsWith("ASB"))
+            {
+                Asb = (D3DBLEND)Byte.Parse(str[i].Split('=')[1]);
+                i++;
+            }
+            if (i < j && str[i].StartsWith("ADB"))
+            {
+                Adb = (D3DBLEND)Byte.Parse(str[i].Split('=')[1]);
+                i++;
+            }
+            if (i < j && str[i].StartsWith("ZBuffer"))
+            {
+                ZBuffer = (Byte.Parse(str[i].Split('=')[1]) != 0);
+                i++;
+            }
+            if (i < j && str[i].StartsWith("Billboard"))
+            {
+                Billboard = (Byte.Parse(str[i].Split('=')[1]) != 0);
+                i++;
+            }
+            return i;
         }
         #endregion
     }
